3a9bffc67525e6d794b70a8ddd63e7e63951a70f
Felice <feliceferri@gmail.com>
2020-09-27 12:17:43 -0400

    Add project files.

---
+++ Code Competitions/Google Kick Start/2020/Round F/ATM Problem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Code_Competitions.Google_Kick_Start._2020.Round_F
+{
+    class ATM_Problem
+    {
+        public class Withdraw
+        {
+            public int originalOrderIndex { get; set; }
+            public int amount { get; set; }
+
+            public int timesNeedsToCycle { get; set; }
+            public Withdraw(int Name, int Amount, int TimesNeedsToCycle)
+            {
+                this.originalOrderIndex = Name;
+                this.amount = Amount;
+                this.timesNeedsToCycle = TimesNeedsToCycle;
+            }
+        }
+        static void Main(string[] args)
+        {
+            TextReader reader = new StreamReader(@".\Google Kick Start\2020\Round F\ATM.txt");
+            //TextReader reader = Console.In;
+            TextWriter writer = Console.Out;
+
+            int testCases = int.Parse(reader.ReadLine());
+
+            for (int testCase = 1; testCase <= testCases; testCase++)
+            {
+                string header = reader.ReadLine();
+                string[] splits = header.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+                int n = Convert.ToInt32(splits[0]);
+                double max = Convert.ToInt32(splits[1]);
+
+                string data = reader.ReadLine();
+                string[] splitsData = data.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                List<Withdraw> list = new List<Withdraw>();
+                int i = 1;
+                foreach (string s in splitsData)
+                {
+                    int amountToWithdraw = Convert.ToInt32(s);
+                    list.Add(new Withdraw(i, amountToWithdraw, Convert.ToInt32(Math.Ceiling(amountToWithdraw / max))));
+                    i++;
+                }
+
+                var sorted = list.OrderBy(x => x.timesNeedsToCycle).ThenBy(x => x.originalOrderIndex);
+
+                Console.WriteLine("Case #" + testCase + ": " + string.Join(" ", sorted.Select(x => x.originalOrderIndex).ToList()));
+
+            }
+        }
+    }
+}
---
+++ Code Competitions/Google Kick Start/2020/Round F/Readme.txt
@@ -0,0 +1,12 @@
+﻿Google Kick Start 2020 Round F
+
+Problem #1  - ATM QUEUE
+https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ff48/00000000003f4ed8
+
+* Note: The statement is presented as a regular queue, but this is about sorting, not about queuing, I code it first as a regular queue and pass the first set of tests, but failed at the second one because Time Exceeding Limit.
+
+The idea basically is create a data structure where you can save the Original place in the line, and also compute the time it has to cycle through the line Math.Ceiling(Amount to withdraw / max allowed to be withrowed)
+Then Sort the data by Number of Cycles and the by Original Place in the line.
+And that's it.
+
+
